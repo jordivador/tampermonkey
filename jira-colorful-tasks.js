657be36309140af9fe277b525c814614695689cb
Jordi Salvador <jordi.salvador@launchmetrics.com>
2020-06-19 15:42:14 +0200

    Added configuration var

--- jira-colorful-tasks.js
+++ jira-colorful-tasks.js
@@ -11,18 +11,24 @@
 // @updateURL    https://raw.githubusercontent.com/jordivador/tampermonkey/master/jira-colorful-tasks.js
 // ==/UserScript==
 
-(function() {
-	setInterval(checkLabels, 30000); // Required for change task colors after ajax changes on page.
+(function () {
+    setInterval(checkLabels, 30000); // Required for change task colors after ajax changes on page.
     setTimeout(checkLabels, 1000);
 })();
+
+const configurations = [
+    { "label": "CD", "color": "#FFB589" },
+    { "label": "Reestimated", "color": "#A2FCBD" }
+];
 
 function checkLabels() {
     document.getElementsByClassName('ghx-issue').forEach(issue => {
         const extraFields = issue.getElementsByClassName('ghx-extra-field');
         extraFields.forEach(extrafield => {
-			const labels = extrafield.getAttribute('data-tooltip').replace('Labels:', '');
-            if (labels.match(/CD/)) return issue.style['background-color'] = '#FFB589'; // orange
-			if (labels.match(/Reestimated/)) return issue.style['background-color'] = '#A2FCBD'; // green
+            const labels = extrafield.getAttribute('data-tooltip').replace('Labels:', '');
+            configurations.forEach(config => {
+                if (labels.match(config.label)) return issue.style['background-color'] = config.color; // orange
+            });
         });
     });
 }
